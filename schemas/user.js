e90783c773451244eaf2e6abaa72f720c6974459
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-08-27 09:47:33 +0900

    timestamp add

--- schemas/user.js
+++ schemas/user.js
@@ -1,37 +1,36 @@
 const mongoose = require("mongoose");
 
-const usersSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true, // 앞뒤 공백제거
-    match: /.+\@.+@..+/,
+const usersSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true, // 앞뒤 공백제거
+      match: /.+\@.+@..+/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    provider: {
+      type: String,
+      required: true,
+      trim: true, // 앞뒤 공백제거
+      // enum : ["1","2","3"]
+    },
+    nickname: {
+      type: String,
+      required: true,
+      trim: true, // 앞뒤 공백제거
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Posts",
+      },
+    ],
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  provider: {
-    type: String,
-    required: true,
-    trim: true, // 앞뒤 공백제거
-    // enum : ["1","2","3"]
-  },
-  nickname: {
-    type: String,
-    required: true,
-    trim: true, // 앞뒤 공백제거
-  },
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Posts",
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    required: true,
-    unique: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Users", usersSchema);
