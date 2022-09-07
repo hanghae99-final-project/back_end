70865cfe1222096d5c1b3c780f4a7c08c56c2251
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-08 00:40:48 +0900

    필요없는 부분 지움

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,5 @@ router.use("/", Admin);
 router.use("/users", Users);
 router.use("/todo", authMiddleware, Todo);
 router.use("/time", authMiddleware, scheduleMiddleware, Time);
-router.get("/sche",getTest);
-function getTest(req, res){
-    let job = schedule.scheduleJob("1 * * * *", function(){
-        console.log("1분마다 스케쥴러가 작동돼!");
-    })
-}
 
 module.exports = router;
