7924f404f2fe9745a28b4f8cfaf54d22856e8ae4
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-08 10:57:44 +0900

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,27 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   ensureAuth: async function (req, res, next) {
     const cookies = req.cookies[process.env.COOKIE_NAME];
-    if (cookies) {
-      const [tokenType, tokenValue] = cookies.split(" ");
-      if (tokenType !== "Bearer") {
-        return res
-          .status(StatusCodes.BAD_REQUEST)
-          .render("alert/alert", { error: "토큰오류." });
+    try {
+      if (cookies) {
+        const [tokenType, tokenValue] = cookies.split(" ");
+        if (tokenType !== "Bearer") {
+          return res
+            .status(StatusCodes.BAD_REQUEST)
+            .render("alert/alert", { error: "토큰오류." });
+        }
+        if (jwt.TokenExpiredError()) {
+          res.clearCookie(process.env.COOKIE_NAME);
+        }
+        const { userId } = jwt.verify(tokenValue, process.env.JWT_SECRET);
+        const admin = await Admin.findOne({ adminEmail: userId });
+        res.locals.user = admin;
+        next();
+      } else {
+        res.redirect("/");
       }
-      const { userId } = jwt.verify(tokenValue, process.env.JWT_SECRET);
-      const admin = await Admin.findOne({ adminEmail: userId });
-      res.locals.user = admin;
-      next();
-    } else {
-      res.redirect("/");
+    } catch (err) {
+      res.clearCookie(process.env.COOKIE_NAME);
+      return res.redirect("/");
     }
   },
   ensureGuest: function (req, res, next) {
