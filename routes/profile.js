7375ff9c24d20489e784ebc1c10667f325e1e520
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-10 04:30:49 +0900

    프로필 수정 페이지 추가 및 todo오류 해결

---
+++ routes/profile.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Profile = require("../controllers/profile");
+
+const asyncWrapper = require("../middleware/async");
+
+router
+  .route("/")
+  .get(asyncWrapper(Profile.getProfile))
+  .put(asyncWrapper(Profile.putProfile));
+
+router.route("/spec").post(asyncWrapper(Profile.postSpec));
+router
+  .route("/spec/:id")
+  .put(asyncWrapper(Profile.putSpec))
+  .delete(asyncWrapper(Profile.deleteSpec));
+
+router
+  .route("/dday")
+  .get(asyncWrapper(Profile.getDday))
+  .post(asyncWrapper(Profile.postDday));
+router
+  .route("/dday/:id")
+  .put(asyncWrapper(Profile.putDday))
+  .delete(asyncWrapper(Profile.deleteDday));
+
+module.exports = router;
