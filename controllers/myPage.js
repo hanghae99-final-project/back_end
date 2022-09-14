634dd4ae0547ce88e6bcde0a9e9276a2bfa9f5b1
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-14 16:04:32 +0900

    date reg added

--- controllers/myPage.js
+++ controllers/myPage.js
@@ -21,9 +21,18 @@ exports.getMyPageMonthly = async (req, res) => {
 
 exports.getMyWeekly = async (req, res) => {
   const user = req.locals;
+  const regex = /\d{4}-\d{2}-\d{2}/;
+
   //Weekly study 데이터
 
   const { startWeek, endWeek } = req.params;
+
+  if (!regex.test(startWeek) || !regex.test(endWeek)) {
+    throw new Error("날짜 형식이 틀립니다.");
+  }
+  if (!endWeek) {
+    throw new Error("검색하는 날짜를 선택해 주세요.");
+  }
 
   const weeklyStudy = await myPageModel.getWeeklyTime(user, startWeek, endWeek);
   return res.status(StatusCodes.OK).json({ weeklyStudy });
