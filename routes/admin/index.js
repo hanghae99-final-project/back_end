5aeff0dc7093a873dafbd11bcc45cbacbe3db8c4
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-20 01:50:26 +0900

    rename: route admin index added

---
+++ routes/admin/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const admin = require("../../controllers/adminDataFunction/login");
+const adminData = require("../../controllers/adminDataFunction/admin");
+const adminPage = require("../../controllers/adminDataFunction/adminPage");
+const { ensureAuth, ensureGuest } = require("../../middleware/auth");
+
+const asyncWrapper = require("../../middleware/async");
+
+router.route("/").get(ensureGuest, asyncWrapper(admin.loginPage)); //   .post(asyncWrapper(admin.create));
+
+router.route("/login").post(asyncWrapper(admin.login));
+router.route("/logout").get(admin.logout);
+router.route("/addObject").post(asyncWrapper(adminData.addObject));
+router
+  .route("/deleteCheckedQuotes")
+  .delete(ensureAuth, asyncWrapper(adminData.deleteCheckedQuotes));
+router.route("/sendCode").post(asyncWrapper(admin.sendCode));
+router.route("/checkCode").post(asyncWrapper(admin.checkCode));
+router.route("/main").get(ensureAuth, adminPage.mainPage);
+router.route("/insQuote").get(ensureAuth, asyncWrapper(adminPage.insQuotePage));
+router.route("/insQuote").post(ensureAuth, asyncWrapper(adminData.insQuote));
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Admin = require("./admin");
+const Admin = require("../routes/admin/index");
 const Users = require("./user");
 const Todo = require("./todo");
 const Time = require("./time");
@@ -22,10 +22,10 @@ router.use("/todo", authMiddleware, Todo);
 router.use("/time", authMiddleware, Time);
 router.use("/rank", authMiddleware, Rank);
 router.use("/mypage", authMiddleware, MyPage);
-router.use("/profile",authMiddleware, Profile);
-router.use("/check",authMiddleware, NickCheck);
+router.use("/profile", authMiddleware, Profile);
+router.use("/check", authMiddleware, NickCheck);
 router.use("/quote", authMiddleware, Quote);
-router.use("/studying",authMiddleware, Studying);
+router.use("/studying", authMiddleware, Studying);
 
 // router.use("/forum", authMiddleware, Forum);
 // router.use("/bookmark",authMiddleware, Bookmark);
