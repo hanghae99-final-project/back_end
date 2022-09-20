381a1d8dce5a37789113833374fe7f375c02b15c
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-21 00:52:02 +0900

    refactor: profile 로직 리팩토링

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,4 @@
-const Profile = require("../models/profile");
-const { profileSchema } = require("../models/userValidation");
+const Profile = require("../service/profile");
 const { StatusCodes } = require("http-status-codes");
 
 exports.getProfile = async (req, res) => {
@@ -10,9 +9,19 @@ exports.getProfile = async (req, res) => {
 
 exports.putProfile = async (req, res) => {
   const user = req.locals;
-  const { nickname, ageGroup, specialty } = await profileSchema.validateAsync(
-    req.body
-  );
+  const { nickname, ageGroup, specialty } = req.body;
+  if(!nickname || !ageGroup || !specialty){
+    throw new Error("입력 값이 없습니다.");
+  }
+  if (typeof(nickname) !== "string"){
+    throw new Error("nickname 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(ageGroup) !== "string"){
+    throw new Error("ageGroup 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(specialty) !== "string"){
+    throw new Error("specialty 형식에 맞지 않습니다.");
+  }
 
   const check = await Profile.putProfile(user, nickname, ageGroup, specialty);
   if (check) {
@@ -22,20 +31,27 @@ exports.putProfile = async (req, res) => {
   }
 };
 
+
 exports.postSpec = async (req, res) => {
   const user = req.locals;
   const { education, career, year, experience } = req.body;
 
-  if(!specId){
-    throw new Error("specId가 없습니다.");
-  }
-  if (education !== "학력" && education !== "경력" || !education) {
-    throw new Error("education 카테고리 입력 오류");
-  }
-  if(!career || !year || !experience){
+  if(!career || !year || !experience || !education){
     throw new Error("입력 값이 없습니다.");
   }
-
+  if (education !== "학력" && education !== "경력") {
+    throw new Error("education 카테고리 입력 오류");
+  }
+  if (typeof(career) !== "string"){
+    throw new Error("career 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(experience) !== "string"){
+    throw new Error("expererience 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(year) !== "string"){
+    throw new Error("year가 형식에 맞지 않습니다.");
+  }
+  
   const createdSpec = await Profile.postSpec(
     user,
     education,
@@ -51,11 +67,23 @@ exports.putSpec = async (req, res) => {
   const specId = req.params.id;
   const { education, career, year, experience } = req.body;
 
-  if (education !== "학력" && education !== "경력" || !education) {
+  if(!specId){
+    throw new Error("specId가 없습니다.");
+  }
+  if(!career || !year || !experience || !education){
+    throw new Error("입력 값이 없습니다.");
+  }
+  if (education !== "학력" && education !== "경력") {
     throw new Error("education 카테고리 입력 오류");
   }
-  if(!career || !year || !experience){
-    throw new Error("입력 값이 없습니다.");
+  if (typeof(career) !== "string"){
+    throw new Error("career 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(experience) !== "string"){
+    throw new Error("expererience 값이 형식에 맞지 않습니다.");
+  }
+  if (typeof(year) !== "string"){
+    throw new Error("year가 형식에 맞지 않습니다.");
   }
 
   const result = await Profile.putSpec(
@@ -84,15 +112,18 @@ exports.deleteSpec = async (req, res) => {
 exports.getDday = async (req, res) => {
   const user = req.locals;
   const myDday = await Profile.getDday(user);
-  myDday.sort((a,b)=> new Date(b.deadline).getTime()- new Date(a.deadline).getTime());
   return res.status(StatusCodes.OK).json({myDday});
 };
 
 exports.postDday = async (req, res) => {
   const user = req.locals;
   const { deadline, content } = req.body;
+  const regex = /\d{4}-\d{2}-\d{2}/;
   if(!deadline || !content){
     throw new Error("dday 또는 내용이 들어가 있지 않습니다.");
+  }
+  if (!regex.test(deadline)) {
+    throw new Error("날짜 형식이 틀립니다.");
   }
 
   const createdDday = await Profile.postDday(user, deadline, content);
@@ -103,11 +134,15 @@ exports.putDday = async (req, res) => {
   const user = req.locals;
   const ddayId = req.params.id;
   const { deadline, content } = req.body;
+  const regex = /\d{4}-\d{2}-\d{2}/;
   if(!ddayId){
     throw new Error("dday Id가 없습니다.")
   }
   if(!deadline || !content){
     throw new Error("dday 또는 내용이 들어가 있지 않습니다.");
+  }
+  if (!regex.test(deadline)) {
+    throw new Error("날짜 형식이 틀립니다.");
   }
 
   const result = await Profile.putDday(user, ddayId, deadline, content);
@@ -118,7 +153,7 @@ exports.deleteDday = async (req, res) => {
   const user = req.locals;
   const ddayId = req.params.id;
   if(!ddayId){
-    throw new Error("dday Id가 없습니다.")
+    throw new Error("dday Id가 없습니다.");
   }
 
   const result = await Profile.deleteDday(user, ddayId);
---
+++ service/profile.js
@@ -0,0 +1,161 @@
+const Profile = require("../models/profile");
+const { profileSchema } = require("../models/userValidation");
+
+exports.getProfile = async (user) => {
+  let myProfile = {};
+  const profile = await Profile.getProfile(user);
+
+  if (profile) {
+    myProfile = {
+      nickname: profile.nickname,
+      ageGroup: profile.ageGroup,
+      specialty: profile.specialty,
+      spec: profile.spec,
+    };
+    return myProfile;
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.putProfile = async (user, nickname, ageGroup, specialty) => {
+  await profileSchema.validateAsync({ nickname, ageGroup, specialty });
+  const existedSameNickname = await Profile.sameNickCheck(user, nickname);
+
+  if (existedSameNickname) {
+    return false;
+  } else {
+    const result = await Profile.putProfile(
+      user,
+      nickname,
+      ageGroup,
+      specialty
+    );
+    return result;
+  }
+};
+
+exports.postSpec = async (user, education, career, year, experience) => {
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.spec.push({ education, career, year, experience });
+    const result = await Profile.saveProfile(myProfile);
+    return result.spec[result.spec.length - 1];
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.putSpec = async (user, specId, education, career, year, experience) => {
+  let specArrIdx = false;
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.spec.map((element, idx) => {
+      if (element._id.equals(specId)) {
+        specArrIdx = idx.toString();
+        myProfile.spec[idx].education = education;
+        myProfile.spec[idx].career = career;
+        myProfile.spec[idx].year = year;
+        myProfile.spec[idx].experience = experience;
+      }
+    });
+    if (!specArrIdx) {
+      throw new Error("spec id가 존재하지 않습니다.");
+    }
+    await Profile.saveProfile(myProfile);
+    return true;
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.deleteSpec = async (user, specId) => {
+  let specArrIdx = false;
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.spec.map((element, idx) => {
+      if (element._id.equals(specId)) {
+        specArrIdx = idx.toString();
+        myProfile.spec.splice(idx, 1);
+      }
+    });
+    if (!specArrIdx) {
+      throw new Error("spec id가 존재하지 않습니다.");
+    }
+    await Profile.saveProfile(myProfile);
+    return true;
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.getDday = async (user) => {
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    const myDday = myProfile.dDay;
+    return myDday.sort(
+      (a, b) => new Date(b.deadline).getTime() - new Date(a.deadline).getTime()
+    );
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.postDday = async (user, deadline, content) => {
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.dDay.push({ deadline, content });
+    const result = await Profile.saveProfile(myProfile);
+    return result.dDay[result.dDay.length - 1];
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.putDday = async (user, ddayId, deadline, content) => {
+  let ddayArrIdx = false;
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.dDay.map((element, idx) => {
+      if (element._id.equals(ddayId)) {
+        ddayArrIdx = idx.toString();
+        myProfile.dDay[idx].deadline = deadline;
+        myProfile.dDay[idx].content = content;
+      }
+    });
+    if (!ddayArrIdx) {
+      throw new Error("D-day id가 존재하지 않습니다.");
+    }
+    await Profile.saveProfile(myProfile);
+    return true;
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
+
+exports.deleteDday = async (user, ddayId) => {
+  let ddayArrIdx = false;
+  const myProfile = await Profile.getProfile(user);
+
+  if (myProfile) {
+    myProfile.dDay.map((element, idx) => {
+      if (element._id.equals(ddayId)) {
+        ddayArrIdx = idx.toString();
+        myProfile.dDay.splice(idx, 1);
+      }
+    });
+    if (!ddayArrIdx) {
+      throw new Error("D-day id가 존재하지 않습니다.");
+    }
+    await Profile.saveProfile(myProfile);
+    return true;
+  } else {
+    throw new Error("프로필이 존재하지 않습니다.");
+  }
+};
