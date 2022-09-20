db3b958f6d6f3d24b3a91465a8cbc6454d320178
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-20 10:51:32 +0900

    fix: scheduler 공부하는 사람이 없을 때 발생하는 버그 수정

--- config/scheduler.js
+++ config/scheduler.js
@@ -7,37 +7,50 @@ exports.scheduler = () => {
   // schedule.scheduleJob("1 * * * * *", () => {
   //     console.log("1분마다 실행");
   //   });
-  
-  const rule = new schedule.RecurrenceRule();
-  rule.hour = 2;
-  rule.tz = "Asia/Seoul";
 
+  const rule = new schedule.RecurrenceRule();
+  rule.hour = 10;
+  rule.minute = 48;
+  rule.tz = "Asia/Seoul";
   schedule.scheduleJob(rule, async () => {
     const today = moment().startOf("day");
     const yesterdayStart = moment(today).subtract(1, "day").add(2, "hours"); // moment(today).add(-1,"days")도 동일
     const yesterdayEnd = moment(yesterdayStart).endOf("day").add(2, "hours");
     const times = await Time.find({
-      // test 위해 today로 바꿔놓았음. 원래는 yesterday
-      createdAt: {
-        $gte: yesterdayStart.toDate(),
-        $lte: yesterdayEnd.toDate(),
-      },
+      $and: [
+        {
+          createdAt: {
+            $gte: yesterdayStart.toDate(),
+            $lte: yesterdayEnd.toDate(),
+          },
+        },
+        {
+          $or: [
+            { studyStartPoint: { $ne: 0 } },
+            { restStartPoint: { $ne: 0 } },
+          ],
+        },
+      ],
     });
-    times.forEach(async (time) => {
-      if (time.studyStartPoint !== 0 && time.restStartPoint === 0) {
-        time.savedStudyTime +=
-          yesterdayEnd.toDate().getTime() - time.studyStartPoint;
-        time.studyStartPoint = 0;
-        await time.save();
-      } else if (time.studyStartPoint === 0 && time.restStartPoint !== 0) {
-        time.savedRestTime +=
-          yesterdayEnd.toDate().getTime() - time.restStartPoint;
-        time.restStartPoint = 0;
-        await time.save();
-      } else {
-        throw new Error("포인트 에러");
-      }
-    });
+
+    if (times.length) {
+      console.log(times);
+      times.forEach(async (time) => {
+        if (time.studyStartPoint !== 0 && time.restStartPoint === 0) {
+          time.savedStudyTime +=
+            yesterdayEnd.toDate().getTime() - time.studyStartPoint;
+          time.studyStartPoint = 0;
+          await time.save();
+        } else if (time.studyStartPoint === 0 && time.restStartPoint !== 0) {
+          time.savedRestTime +=
+            yesterdayEnd.toDate().getTime() - time.restStartPoint;
+          time.restStartPoint = 0;
+          await time.save();
+        } else {
+          console.error("포인트에러");
+        }
+      });
+    }
     await Studying.deleteMany({});
   });
 };
