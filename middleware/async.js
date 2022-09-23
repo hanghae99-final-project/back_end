4b7d386a49f16cc57eaed06eb267455bb23d2303
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-23 14:19:18 +0900

    fix: admin login bug fix

--- middleware/async.js
+++ middleware/async.js
@@ -2,10 +2,14 @@ const logger = require("../config/logger");
 const asyncWrapper = (fn) => {
   return async (req, res, next) => {
     try {
-      const user = req.locals;
-      logger.info(`req : ${user.nickname}(${req.ip}), method: ${req.method}, api: ${req.url}`);
+      if(req.locals){
+        const user = req.locals;
+        logger.info(`req : ${user.nickname}(${req.ip}), method: ${req.method}, api: ${req.url}`);
+        await fn(req, res, next);
+        logger.info(`res : ${user.nickname}(${req.ip}), status: ${res.statusCode}(${res.statusMessage})`);
+      }
       await fn(req, res, next);
-      logger.info(`res : ${user.nickname}(${req.ip}), status: ${res.statusCode}(${res.statusMessage})`);
+      
     } catch (error) {
       //passing the next middleware.
       next(error);
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,13 +1,23 @@
 const { StatusCodes } = require("http-status-codes");
+const { reset } = require("nodemon");
 const logger = require("../config/logger");
 exports.errorHandlerMiddleware = (err, req, res, next) => {
-    const user = req.locals;
-    if(!err.isBoom){
-        logger.error(`res : ${user.nickname}(${req.ip}), ${StatusCodes.INTERNAL_SERVER_ERROR} : ${err.name} - ${err.message}`);
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({errMessage: err.message});
+    try{
+        if(!err.isBoom){
+            if(req.locals){
+                logger.error(`res : ${user.nickname}(${req.ip}), ${StatusCodes.INTERNAL_SERVER_ERROR} : ${err.name} - ${err.message}`);
+            }
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({errMessage: err.message});
+        }
+        else{
+            if(req.locals){
+                logger.error(`res : ${user.nickname}(${req.ip}), ${err.output.statusCode} : ${err.name} - ${err.output.payload.message}`);
+            } 
+            return res.status(err.output.statusCode).json({errorMessage : err.output.payload.message});
+        }
     }
-    else{
-        logger.error(`res : ${user.nickname}(${req.ip}), ${err.output.statusCode} : ${err.name} - ${err.output.payload.message}`);
-        return res.status(err.output.statusCode).json({errorMessage : err.output.payload.message});
+    catch(err){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err});
     }
+
 }
