95228127762285c6e90d2da6cb591b6a6f4deb98
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-27 23:38:28 +0900

    fix: eslint 수정

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
