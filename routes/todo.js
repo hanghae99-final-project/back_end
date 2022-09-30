0de556a1ab2e0a25fb645bcfbfbc55cd725d79b6
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-30 18:17:49 +0900

    rename: "controllers/todo -> controller/todo.controller"

--- routes/todo.js
+++ routes/todo.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const asyncWrapper = require("../middleware/async");
-const TodoController = require("../controllers/todo");
+const TodoController = require("../controllers/todo.controller");
 
+router.route("/").post(asyncWrapper(TodoController.createTodo));
+router.route("/:day").get(asyncWrapper(TodoController.getTodo));
 router
-  .route("/")
-  .post(asyncWrapper(TodoController.createTodo));
-router.route("/:day")
-  .get(asyncWrapper(TodoController.getTodo));
-router.route("/:id")
+  .route("/:id")
   .put(asyncWrapper(TodoController.putTodo))
   .delete(asyncWrapper(TodoController.deleteTodo));
 
