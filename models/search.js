f9c2d8784e75e60ef688dafec0f2e66c0fb03c6d
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-30 18:46:18 +0900

    remove: search

--- models/search.js
+++
@@ -1,8 +0,0 @@
-const user = require("../schemas/user");
-
-exports.findSearch = async (word) => {
-  const result = await user.find({
-    nickname: { $regex: word, $options: "i" },
-  });
-  return result;
-};
