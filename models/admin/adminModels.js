47a5cc81cf08b5c2e88b846dc0abd86d4b761144
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-30 18:54:56 +0900

    remove admin

--- models/admin/adminModels.js
+++
@@ -1,9 +0,0 @@
-const User = require("../../schemas/user");
-
-exports.getUserInfoModel = async (userId) => {
-  const result = await User.findById({ _id: userId });
-  return result;
-};
-exports.getAllUsers = async () => {
-  return await User.find({});
-};
--- models/adminLogin.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-exports.comparePassword = async (candidatePassword, comparePassword) => {
-  const isMatch = await bcrypt.compare(candidatePassword, comparePassword);
-  return isMatch;
-};
