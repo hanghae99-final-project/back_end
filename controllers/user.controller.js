94c343f541d531046b80d3097cda7c81db1c2678
CalvinParkNov <dev.calvinpark@gmail.com>
2022-09-30 18:13:11 +0900

    rename:"user.controller.js"

---
+++ controllers/user.controller.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+const userService = require("../service/user.service");
+const { StatusCodes } = require("http-status-codes");
+const User = require("../schemas/user");
+const userModel = require("../models/login");
+const { ConflictError } = require("../errors");
+
+exports.kakaoCallbackLocal = (req, res, next) => {
+  passport.authenticate(
+    "kakao",
+    { failureRedirect: "/" },
+    async (err, user) => {
+      if (err) return next(err);
+
+      const { kakaoId } = user;
+      const userInfo = await User.findOne({ kakaoId });
+
+      const token = userModel.createJWT(userInfo);
+      res.status(StatusCodes.OK).json({ token });
+    }
+  )(req, res, next);
+};
+
+//회원 수정
+exports.modProfile = async (req, res) => {
+  const user = req.locals;
+  //닉네임, 연령별, 전문분야 선택 필수 입력.
+  const { nickname, ageGroup, specialty } = req.body;
+  await userService.nicknameCheck(nickname);
+  await userService.ageGroupCheck(ageGroup);
+  await userService.specialtyCheck(specialty);
+
+  //닉네임 저장
+  const check = await userService.checkNickname(
+    user._id,
+    nickname,
+    ageGroup,
+    specialty
+  );
+
+  if (check) {
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "회원가입이 완료되었습니다." });
+  } else {
+    throw new ConflictError("닉네임이 중복 됩니다.");
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/userAuth");
-const User = require("../controllers/user");
+const User = require("../controllers/user.controller");
 const asyncWrapper = require("../middleware/async");
 
 // router.route("/kakao/finish").get(User.kakaoCallback);
