0312aeda2011e40870f591493fdc52ef3b989adb
CalvinParkNov <dev.calvinpark@gmail.com>
2022-10-01 19:22:42 +0900

    fix: notification

--- config/notification.js
+++ config/notification.js
@@ -60,26 +60,22 @@ exports.scheduler = () => {
               notification: {
                 title: "랭플", //메세지 제목
                 body: "안쪽", //메세지 내용
-                click_action: "https://ranking-planner.com", //클릭시 홈페이지 이동
-                icon: "/firebase-logo.png", //사용자 프로필 오면 좋을듯
               },
             };
 
-            schedule.scheduleJob("setTime", "* * * * * *", async () => {
-              console.log("메세지 전송 성공");
-              //fcm서버에 http 통신을 통해 위 알림내용 보내기
-              await fetch("https://fcm.googleapis.com/fcm/send", {
-                method: "post",
-                body: JSON.stringify(body),
-                headers: {
-                  //보내는 양식
-                  "Content-Type": "application/json",
-                  //서버키 입력
-                  Authorization: `key=${process.env.FIREBASE_Authorization}`,
-                },
-              });
-              schedule.cancelJob("setTime");
+            console.log("메세지 전송 성공");
+            //fcm서버에 http 통신을 통해 위 알림내용 보내기
+            await fetch("https://fcm.googleapis.com/fcm/send", {
+              method: "post",
+              body: JSON.stringify(body),
+              headers: {
+                //보내는 양식
+                "Content-Type": "application/json",
+                //서버키 입력
+                Authorization: `key=${process.env.FIREBASE_Authorization}`,
+              },
             });
+            schedule.cancelJob("setTime");
           } catch (e) {
             console.log(e);
           }
